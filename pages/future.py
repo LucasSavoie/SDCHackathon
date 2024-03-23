d42ef315f74a5246558c69aef1d4995c8833b27a
Aidan Foster <132408783+aidanfoster1@users.noreply.github.com>
2024-03-23 14:12:13 -0300

    Update future.py

--- pages/future.py
+++ pages/future.py
@@ -15,13 +15,13 @@ with col1:
    st.page_link("app.py", label="Home", icon="🏠")
 
 with col2:
-   st.page_link("pages/future_costs.py", label="Predicted Future Costs")
+   st.page_link("pages/future_costs.py", label="View Future Predictions")
 
 with col3:
    st.page_link("pages/historical.py", label="View Histroical Data")
 
 with col4:
-   st.page_link("pages/future.py", label="View Future Predictions")
+   st.page_link("pages/future.py", label="Predicted Future Costs")
 
 
 
