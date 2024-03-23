1c47547723131bca55d77c2d7c099e8eebbf2eb5
Aidan Foster <132408783+aidanfoster1@users.noreply.github.com>
2024-03-23 13:59:27 -0300

    Update app.py
    Adding description to homepage

--- app.py
+++ app.py
@@ -12,8 +12,17 @@ left_co, cent_co,last_co = st.columns(3)
 with cent_co:
     st.image('Irving_Oil.svg.png',width= 150)
 
-st.title('Irvine Oil Carbon Challenge')
+st.title('EnviroVision')
+st.markdown("""
+    #Problem Description
+    The task is to design and implement an application for Irvine Gasoline, a large oil and gas company, aiming to transition to carbon neutrality after years of neglecting their carbon emissions. The application should assist Irvine Gasoline in managing and achieving their goal of becoming carbon neutral. 
 
+    #How does EnviroVision solve this problem?
+    1. Analyzes previous carbon emission data, providing insights into historical environmental impact.
+    2. By leveraging ML and predictive analytics, EnviroVision forecasts future emission trends, aiding Irvine Gasoline in setting achievable carbon neutrality goals.
+    3. Additionally, the EnviroVision offers tools for evaluating the effectiveness of different emission reduction strategies and selecting optimal carbon offsetting options.
+
+""")
 col1, col2, col3, col4 = st.columns(4)
 with col1:
    st.page_link("app.py", label="Home", icon="🏠")
